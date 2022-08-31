e6a2ac13b15183daf889dd56457a2aa4c2ccecc8
HeorhiiSmirnov <georgiysmirnofff@gmail.com>
2022-08-31 21:12:07 +0300

    delete

--- core/phone_book/views/delete.py
+++ core/phone_book/views/delete.py
@@ -7,11 +7,3 @@ class PhoneBookDeleteView(DeleteView):
     model = Phone_book
     template_name = 'phone_book_confirm_delete.html'
     success_url = reverse_lazy('show_all')
-
-
-
-
-# def delete(request, pk):
-#     total_deleted, _ = Phone_book.objects.filter(pk=pk).delete()
-
-#     return reverse_lazy('show_all')
